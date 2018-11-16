019f5070a44b3d328f9d052601ef4d2a48bf3f2e
Rory Hager <roryy18@gmail.com>
2018-11-16 15:02:42 -0600

    further refactor using props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Sam"
+        time="Today at 4:45PM"
+        text="Great post, Jenny!"
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Alex"
+        time="Today at 2:00AM"
+        text="I disagree! This article is very misleading!"
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Jane"
+        time="Yesterday at 5:20PM"
+        text="Alex is salty. Awesome article!"
+      />
     </div>
   );
 };
